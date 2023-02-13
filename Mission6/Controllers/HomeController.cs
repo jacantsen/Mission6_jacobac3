241ae62cdce7dd9888dfcc65c260fc7bafd0a1a5
jacantsen <jacob.ac@sbcglobal.net>
2023-02-13 15:33:33 -0700

    Add project files.

---
+++ Mission6/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieFormContext MovieContext { get; set; }
+        public HomeController(ILogger<HomeController> logger, MovieFormContext someName)
+        {
+            _logger = logger;
+            MovieContext = someName;
+        }
+        //show the home page
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // show the podcast page
+        [HttpGet]
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+        // show the movie form
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+        //post the movie form data
+        [HttpPost]
+        public IActionResult MovieForm(MovieFormResponse movie)
+            // save the form data into the database
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                MovieContext.Add(movie);
+                MovieContext.SaveChanges();
+
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+            public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission6/Models/MovieFormContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Mission6.Models
+{
+    public class MovieFormContext : DbContext
+    {
+        public MovieFormContext(DbContextOptions<MovieFormContext> options) : base(options)
+        {
+            //leave blank for now
+        }
+        public DbSet<MovieFormResponse> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieFormResponse>().HasData(
+                // add 3 entries into the database
+                new MovieFormResponse
+                {
+                    ApplicationID = 1,
+                    Category = "Action, Adventure, Drama",
+                    Title = "The Lord of the Rings: The Fellowship of the Ring",
+                    Year = "2001",
+                    Director = "Peter Jackson",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "Nobody",
+                    Notes = "Great movie!"
+                },
+                new MovieFormResponse
+                {
+                    ApplicationID = 2,
+                    Category = "Action, Adventure, Drama",
+                    Title = "The Lord of the Rings: The Two Towers",
+                    Year = "2002",
+                    Director = "Peter Jackson",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "Nobody",
+                    Notes = "Has some great action scenes!"
+                },
+                new MovieFormResponse
+                {
+                    ApplicationID = 3,
+                    Category = "Action, Adventure, Drama",
+                    Title = "The Lord of the Rings: The Return of the Ring",
+                    Year = "2003",
+                    Director = "Peter Jackson",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "Nobody",
+                    Notes = "Words cannot describe how great this movie is!"
+                }
+
+                );
+        }
+    }
+}
+
